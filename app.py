4347946902dc492eb3484e0cc8ce695cd2890c56
KOTBI Ahmed <allalchakir7@gmail.com>
2025-04-02 23:29:56 +0200

    Correction affichage de l'image

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ T = 300.0  # Température (K)
 
 # Titre de l'application
 st.title("Calcul des paramètres du modèle équivalent d'une cellule solaire")
+# Affichage d'une image explicative
+st.image("figure_ok.png", caption="Caractéristiques I-V d'une cellule solaire", use_column_width=True)
 
 # Entrée des paramètres par l'utilisateur
 Isc = st.number_input("Courant de court-circuit (Isc) [A]", min_value=0.0)
 Voc = st.number_input("Tension à vide (Voc) [V]", min_value=0.0)
 Vm = st.number_input("Tension au point de puissance max (Vm) [V]", min_value=0.0)
 Im = st.number_input("Courant au point de puissance max (Im) [A]", min_value=0.0)
+
 
 V1_Rs0 = st.number_input("V1 pour Rs [V]", min_value=0.0)
 I1_Rs0 = st.number_input("I1 pour Rs [A]", min_value=0.0)
